49547662ed93845dc98dff344f1b0efc24321543
michaelsatumba <mjsatumba@gmail.com>
2023-06-15 14:23:41 -0700

    time is working

--- time.js
+++ time.js
@@ -1,19 +1,22 @@
-// Function to update the day and time in the specified time zone
-function updateTime(dayElement, timeElement, timeZone) {
-  const now = new Date();
-  const timeZoneOptions = {
-    timeZone: timeZone,
-    hour12: true,
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-  };
-  const day = now.toLocaleString('en-US', { weekday: 'long' });
-  const time = now.toLocaleString('en-US', timeZoneOptions);
-  dayElement.textContent = day;
-  timeElement.textContent = time;
-}
 
+    // Function to update the time in the specified time zone
+    function updateTime(timeZoneElement, timeZone) {
+      const now = new Date();
+      const timeZoneOptions = {
+        timeZone: timeZone,
+        weekday: 'long',
+        hour12: true,
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+      };
+      const time = now.toLocaleString('en-US', timeZoneOptions);
+      timeZoneElement.textContent = time;
+    }
+    
+    
+
+// Array of time zones
 const timeZones = [
   { label: 'Calabasas', timeZone: 'America/Los_Angeles', image: 'pngwing.com.png' },
   { label: 'Tempe', timeZone: 'America/Phoenix', image: 'pngwing.com.png' },
@@ -26,25 +29,20 @@ const timeZones = [
   { label: 'Hawaii', timeZone: 'Pacific/Honolulu', image: 'pngwing.com.png' }
 ];
 
-
 const timeZonesContainer = document.getElementById('timeZones');
 
 // Create elements for each time zone
 timeZones.forEach(({ label, timeZone, image }, index) => {
   const timeZoneElement = document.createElement('div');
   timeZoneElement.className = 'timeZone';
-
   const heading = document.createElement('h2');
   heading.textContent = label;
   timeZoneElement.appendChild(heading);
 
-  const dayElement = document.createElement('p');
-  dayElement.className = 'day';
-  const timeElement = document.createElement('p');
-  timeElement.className = 'time';
-  timeZoneElement.appendChild(dayElement);
-  timeZoneElement.appendChild(timeElement);
+  const time = document.createElement('p');
+  timeZoneElement.appendChild(time);
   timeZonesContainer.appendChild(timeZoneElement);
+  updateTime(time, timeZone);
 
   const imgPath = `${image}`;
   const imageElement = document.createElement('img');
@@ -58,20 +56,15 @@ timeZones.forEach(({ label, timeZone, image }, index) => {
   } else {
     timeZoneElement.classList.add('odd');
   }
-
-  updateTime(dayElement, timeElement, timeZone);
 });
 
-function updateAllTimeZones() {
-  const timeZoneElements = Array.from(document.querySelectorAll('.timeZone'));
-  timeZoneElements.forEach((timeZoneElement) => {
-    const dayElement = timeZoneElement.querySelector('p:first-child');
-    const timeElement = timeZoneElement.querySelector('p:last-child');
-    const label = timeZoneElement.querySelector('h2').textContent;
-    const timeZone = timeZones.find(({ label: timeZoneLabel }) => timeZoneLabel === label).timeZone;
-    updateTime(dayElement, timeElement, timeZone);
+    function updateAllTimeZones() {
+  timeZones.forEach(({ label, timeZone }) => {
+    const timeZoneElement = Array.from(document.querySelectorAll('.timeZone h2')).find(h2 => h2.textContent === label).nextElementSibling;
+    updateTime(timeZoneElement, timeZone);
   });
 }
 
-// Schedule periodic updates every second
-setInterval(updateAllTimeZones, 1000);
+
+    // Schedule periodic updates every second
+    setInterval(updateAllTimeZones, 1000);
